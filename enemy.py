7b4ec2154b3aa0592518e6f4dab5877c45f9e116
mewpk <milzaza123@gmail.com>
2022-11-08 01:43:21 +0700

    Successful enemy!

--- enemy.py
+++ enemy.py
@@ -1,7 +1,44 @@
 import pygame
+from random import randint
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, screen_width, screen_height):
+    def __init__(self, pos,screen_width,screen_height):
         super().__init__()
-        self.screen_width = screen_width
-        self.screen_height = screen_height
+        self.screen_width, self.screen_height = screen_width , screen_height
+        vec = pygame.math.Vector2
+        run1 = pygame.image.load('graphics/sprites/enemy/dragon_down1.png').convert_alpha()
+        run2 = pygame.image.load('graphics/sprites/enemy/dragon_down2.png').convert_alpha()
+        run3 = pygame.image.load('graphics/sprites/enemy/dragon_down3.png').convert_alpha()
+        run4 = pygame.image.load('graphics/sprites/enemy/dragon_down4.png').convert_alpha()
+        # run5 = pygame.image.load('graphics/sprites/enemy/run/5.png').convert_alpha()
+        # run6 = pygame.image.load('graphics/sprites/enemy/run/6.png').convert_alpha()
+        # run7 = pygame.image.load('graphics/sprites/enemy/run/7.png').convert_alpha()
+        # run8 = pygame.image.load('graphics/sprites/enemy/run/8.png').convert_alpha()
+        self.run = [run1, run2, run3, run4]
+
+        self.frame_index = 0
+        self.image = self.run[self.frame_index]
+        self.rect = self.image.get_rect(center=(pos))
+
+        self.enemy_speed = 1
+        self.vel = vec(0, 0)
+
+    def running(self):
+        self.frame_index += 0.15
+        if self.frame_index >= len(self.run):
+            self.frame_index = 0
+        self.image = self.run[int(self.frame_index)]
+
+    def constraint(self):
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= self.screen_width:
+            self.rect.right = self.screen_width
+        if self.rect.top <= 20:
+            self.rect.top = 20
+        if self.rect.bottom >= self.screen_height:
+            self.rect.bottom = self.screen_height
+    
+    def update(self):
+        self.running()
+        self.constraint()
--- gamerun.py
+++ gamerun.py
@@ -1,5 +1,7 @@
 import pygame
+from random import randint, choice
 from player import Player
+from enemy import Enemy
 
 class Gamerun():
     def __init__(self, screen, screen_width, screen_height):
@@ -10,8 +12,35 @@ class Gamerun():
         #player
         player_sprite = Player(self.screen_width, self.screen_height)
         self.player = pygame.sprite.GroupSingle(player_sprite)
-    
+        
+        # enemy
+        self.random_x = choice([0, 600])
+        self.random_y = randint(30,370)
+        enemy_sprite = Enemy((self.random_x, self.random_y),self.screen_width, self.screen_height)
+        self.enemy = pygame.sprite.Group(enemy_sprite)
+        self.enemy_adding_time = 100
+
+    def enemy_move(self):
+        for enemy in self.enemy:
+            if enemy.rect.x >= self.player.sprite.rect.x:
+                enemy.rect.x -= 1
+            if enemy.rect.x < self.player.sprite.rect.x:
+                enemy.rect.x += 1
+            if enemy.rect.y >= self.player.sprite.rect.y:
+                enemy.rect.y -= 1
+            if enemy.rect.y < self.player.sprite.rect.y:
+                enemy.rect.y += 1
+    def add_enemy(self):
+        self.enemy_adding_time -= 1
+        if self.enemy_adding_time <= 0:
+            self.enemy.add(Enemy((choice([-70, 650]), randint(70,300))))
+            self.get_enemy_adding_time_new()
+            self.enemy_adding_time_new  = randint(80, 180)
+            self.enemy_adding_time = self.enemy_adding_time_new
     def run(self):
         self.screen.fill("white")
         self.player.draw(self.screen)
+        self.enemy.draw(self.screen)
         self.player.update()
+        self.enemy.update()
+        self.enemy_move()
