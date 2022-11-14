09f561a61ca082ab2839082657f6802fcb6dec41
mewpk <milzaza123@gmail.com>
2022-11-14 16:22:31 +0700

    add score and hp bar !

--- gamerun.py
+++ gamerun.py
@@ -1,16 +1,22 @@
-import pygame, sys
-from random import randint, choice
-from player import Player
+import random
+import sys
+from random import choice, randint
+
+import pygame
+
 from enemy import Enemy
 from enemy2 import Enemy2
 from enemy3 import Enemy3
-import random
+from player import Player
+
 
 def display_text(text, size, color, pos, screen):
     font = pygame.font.Font('graphics/font/PressStart2P-vaV7.ttf', size)
     text_surf = font.render(f'{text}', False, color)
-    text_rect = text_surf.get_rect(center = pos)
+    text_rect = text_surf.get_rect(center=pos)
     screen.blit(text_surf, text_rect)
+
+
 class Gamerun():
     def __init__(self, screen, screen_width, screen_height):
         self.screen = screen
@@ -24,130 +30,171 @@ class Gamerun():
         player_sprite = Player(self.screen_width, self.screen_height)
         self.player = pygame.sprite.GroupSingle(player_sprite)
         self.life = 10
+        self.lifebox = pygame.Surface((20, 20))
+        self.lifebox.fill('red')
+        self.life_bg = pygame.Surface((200, 20))
+        self.life_bg.fill('black')
+        self.game_status = 1
 
-        #sound 
+        # sound
         self.hit = pygame.mixer.Sound("graphics/music/hit.mp3")
         self.hit.set_volume(2)
-        
+
         # money
         self.money = 0
         self.score = 0
         self.money_upgrade_bullet = [2000, 3000, 4000, 5000, 10000, "MAX"]
         # self.money_upgrade_bullet = [, 1, 2, 1, 2, 'max']
         self.money_upgrade_bullet_i = 0
-        
+
         # enemy
-        self.enemy_sprite = Enemy((choice([0, self.screen_width]), randint(0, self.screen_height)),self.screen_width, self.screen_height, self.player.sprite.rect)
+        self.enemy_sprite = Enemy((choice([0, self.screen_width]), randint(
+            0, self.screen_height)), self.screen_width, self.screen_height, self.player.sprite.rect)
         self.enemy = pygame.sprite.Group(self.enemy_sprite)
         self.enemy_adding_time = 150
         self.enemy_kill = 0
         # button
-        self.button_money = pygame.Rect((self.screen_width - 150, 80), (80, 40))
+        self.button_money = pygame.Rect(
+            (self.screen_width - 160, 80), (100, 40))
 
     def enemy_move(self):
         for enemy in self.enemy:
-            if enemy.rect.x > self.player.sprite.rect.x:
-                enemy.rect.x -= 1
-            elif enemy.rect.x < self.player.sprite.rect.x:
-                enemy.rect.x += 1
-                
-            if enemy.rect.y > self.player.sprite.rect.y:
-                enemy.rect.y -= 1
-            elif enemy.rect.y < self.player.sprite.rect.y:
-                enemy.rect.y += 1
+
+            x = choice((0, 1))
+            if x == 0:
+                if enemy.rect.x > self.player.sprite.rect.x:
+                    enemy.rect.x -= 1
+                elif enemy.rect.x < self.player.sprite.rect.x:
+                    enemy.rect.x += 1
+            elif x == 1:
+                if enemy.rect.y > self.player.sprite.rect.y:
+                    enemy.rect.y -= 1
+                elif enemy.rect.y < self.player.sprite.rect.y:
+                    enemy.rect.y += 1
 
     def add_enemy(self):
         self.enemy_adding_time -= 1
         if self.enemy_adding_time <= 0:
             x_or_y = random.randrange(1, 7)
             if x_or_y == 1:
-                self.enemy.add(Enemy((choice([0, self.screen_width]), randint(0, self.screen_height)),self.screen_width, self.screen_height, self.player.sprite.rect))
+                self.enemy.add(Enemy((choice([0, self.screen_width]), randint(
+                    0, self.screen_height)), self.screen_width, self.screen_height, self.player.sprite.rect))
             elif x_or_y == 2:
-                self.enemy.add(Enemy((randint(0, self.screen_width), choice([0, self.screen_height])),self.screen_width, self.screen_height, self.player.sprite.rect))
-            elif x_or_y == 3 :
-                self.enemy.add(Enemy2((choice([0, self.screen_width]), randint(0, self.screen_height)),self.screen_width, self.screen_height, self.player.sprite.rect))
-            elif x_or_y == 4 :
-                self.enemy.add(Enemy2((randint(0, self.screen_width), choice([0, self.screen_height])),self.screen_width, self.screen_height, self.player.sprite.rect))
-            elif x_or_y == 5 :
-                self.enemy.add(Enemy3((choice([0, self.screen_width]), randint(0, self.screen_height)),self.screen_width, self.screen_height, self.player.sprite.rect))
-            elif x_or_y == 6 :
-                self.enemy.add(Enemy3((randint(0, self.screen_width), choice([0, self.screen_height])),self.screen_width, self.screen_height, self.player.sprite.rect))
-            if self.bullet_type == 5 :
+                self.enemy.add(Enemy((randint(0, self.screen_width), choice(
+                    [0, self.screen_height])), self.screen_width, self.screen_height, self.player.sprite.rect))
+            elif x_or_y == 3:
+                self.enemy.add(Enemy2((choice([0, self.screen_width]), randint(
+                    0, self.screen_height)), self.screen_width, self.screen_height, self.player.sprite.rect))
+            elif x_or_y == 4:
+                self.enemy.add(Enemy2((randint(0, self.screen_width), choice(
+                    [0, self.screen_height])), self.screen_width, self.screen_height, self.player.sprite.rect))
+            elif x_or_y == 5:
+                self.enemy.add(Enemy3((choice([0, self.screen_width]), randint(
+                    0, self.screen_height)), self.screen_width, self.screen_height, self.player.sprite.rect))
+            elif x_or_y == 6:
+                self.enemy.add(Enemy3((randint(0, self.screen_width), choice(
+                    [0, self.screen_height])), self.screen_width, self.screen_height, self.player.sprite.rect))
+            if self.bullet_type == 5:
                 self.enemy_adding_time = 10
-                if random.randrange(0,3) == 0:
-                    for i in range(1,5):
+                if random.randrange(0, 3) == 0:
+                    for i in range(1, 5):
                         x_or_y = random.randrange(1, 7)
                         if x_or_y == 1:
-                            self.enemy.add(Enemy((choice([0, self.screen_width]), randint(0, self.screen_height)),self.screen_width, self.screen_height, self.player.sprite.rect))
+                            self.enemy.add(Enemy((choice([0, self.screen_width]), randint(
+                                0, self.screen_height)), self.screen_width, self.screen_height, self.player.sprite.rect))
                         elif x_or_y == 2:
-                            self.enemy.add(Enemy((randint(0, self.screen_width), choice([0, self.screen_height])),self.screen_width, self.screen_height, self.player.sprite.rect))
-                        elif x_or_y == 3 :
-                            self.enemy.add(Enemy2((choice([0, self.screen_width]), randint(0, self.screen_height)),self.screen_width, self.screen_height, self.player.sprite.rect))
-                        elif x_or_y == 4 :
-                            self.enemy.add(Enemy2((randint(0, self.screen_width), choice([0, self.screen_height])),self.screen_width, self.screen_height, self.player.sprite.rect))
-                        elif x_or_y == 5 :
-                            self.enemy.add(Enemy3((choice([0, self.screen_width]), randint(0, self.screen_height)),self.screen_width, self.screen_height, self.player.sprite.rect))
-                        elif x_or_y == 6 :
-                            self.enemy.add(Enemy3((randint(0, self.screen_width), choice([0, self.screen_height])),self.screen_width, self.screen_height, self.player.sprite.rect))
-            elif self.score >= 100000 :
+                            self.enemy.add(Enemy((randint(0, self.screen_width), choice(
+                                [0, self.screen_height])), self.screen_width, self.screen_height, self.player.sprite.rect))
+                        elif x_or_y == 3:
+                            self.enemy.add(Enemy2((choice([0, self.screen_width]), randint(
+                                0, self.screen_height)), self.screen_width, self.screen_height, self.player.sprite.rect))
+                        elif x_or_y == 4:
+                            self.enemy.add(Enemy2((randint(0, self.screen_width), choice(
+                                [0, self.screen_height])), self.screen_width, self.screen_height, self.player.sprite.rect))
+                        elif x_or_y == 5:
+                            self.enemy.add(Enemy3((choice([0, self.screen_width]), randint(
+                                0, self.screen_height)), self.screen_width, self.screen_height, self.player.sprite.rect))
+                        elif x_or_y == 6:
+                            self.enemy.add(Enemy3((randint(0, self.screen_width), choice(
+                                [0, self.screen_height])), self.screen_width, self.screen_height, self.player.sprite.rect))
+            elif self.score >= 100000:
                 self.enemy_adding_time = 30
-            elif self.score >= 80000 :
+            elif self.score >= 80000:
                 self.enemy_adding_time = 40
-            elif self.score >= 60000 :
+            elif self.score >= 60000:
                 self.enemy_adding_time = 60
-            elif self.score >= 40000 :
+            elif self.score >= 40000:
                 self.enemy_adding_time = 80
-            elif self.score >= 30000 :
+            elif self.score >= 30000:
                 self.enemy_adding_time = 90
-            elif self.score >= 20000 :
+            elif self.score >= 20000:
                 self.enemy_adding_time = 100
-            elif self.score >= 10000 :
+            elif self.score >= 10000:
                 self.enemy_adding_time = 120
-            elif self.score >= 0 :
+            elif self.score >= 0:
                 self.enemy_adding_time = 130
-            
-            if random.randrange(0,5) == 0:
-                for i in range(1,7):
+
+            if random.randrange(0, 5) == 0:
+                for i in range(1, 7):
                     x_or_y = random.randrange(1, 5)
                     if x_or_y == 1:
-                        self.enemy.add(Enemy((choice([0, self.screen_width]), randint(0, self.screen_height)),self.screen_width, self.screen_height, self.player.sprite.rect))
+                        self.enemy.add(Enemy((choice([0, self.screen_width]), randint(
+                            0, self.screen_height)), self.screen_width, self.screen_height, self.player.sprite.rect))
                     elif x_or_y == 2:
-                        self.enemy.add(Enemy((randint(0, self.screen_width), choice([0, self.screen_height])),self.screen_width, self.screen_height, self.player.sprite.rect))
-                    elif x_or_y == 3 :
-                        self.enemy.add(Enemy2((choice([0, self.screen_width]), randint(0, self.screen_height)),self.screen_width, self.screen_height, self.player.sprite.rect))
-                    elif x_or_y == 4 :
-                        self.enemy.add(Enemy2((randint(0, self.screen_width), choice([0, self.screen_height])),self.screen_width, self.screen_height, self.player.sprite.rect))
-                    elif x_or_y == 5 :
-                        self.enemy.add(Enemy3((choice([0, self.screen_width]), randint(0, self.screen_height)),self.screen_width, self.screen_height, self.player.sprite.rect))
-                    elif x_or_y == 6 :
-                        self.enemy.add(Enemy3((randint(0, self.screen_width), choice([0, self.screen_height])),self.screen_width, self.screen_height, self.player.sprite.rect))
-            
-    
+                        self.enemy.add(Enemy((randint(0, self.screen_width), choice(
+                            [0, self.screen_height])), self.screen_width, self.screen_height, self.player.sprite.rect))
+                    elif x_or_y == 3:
+                        self.enemy.add(Enemy2((choice([0, self.screen_width]), randint(
+                            0, self.screen_height)), self.screen_width, self.screen_height, self.player.sprite.rect))
+                    elif x_or_y == 4:
+                        self.enemy.add(Enemy2((randint(0, self.screen_width), choice(
+                            [0, self.screen_height])), self.screen_width, self.screen_height, self.player.sprite.rect))
+                    elif x_or_y == 5:
+                        self.enemy.add(Enemy3((choice([0, self.screen_width]), randint(
+                            0, self.screen_height)), self.screen_width, self.screen_height, self.player.sprite.rect))
+                    elif x_or_y == 6:
+                        self.enemy.add(Enemy3((randint(0, self.screen_width), choice(
+                            [0, self.screen_height])), self.screen_width, self.screen_height, self.player.sprite.rect))
+
     def display_money(self):
-        display_text(f'Money :{self.money} $', 20, 'black', (self.screen_width - 200, 50), self.screen)
+        display_text(f'Money :{self.money} $', 20, 'black',
+                     (self.screen_width - 200, 50), self.screen)
+
     def display_score(self):
-        display_text(f'Score :{self.score}', 20, 'black', (self.screen_width/2, 50), self.screen)
+        display_text(f'Score :{self.score}', 20, 'black',
+                     (self.screen_width/2, 50), self.screen)
+
     def display_hp(self):
-        display_text(f'HP :{self.life}', 20, 'black', (100, 50), self.screen)
+        if self.game_status == 1:
+            self.screen.blit(self.life_bg, (50, 40))
+            for each_life in range(self.life):
+                x = 50 + (each_life * (self.lifebox.get_size()[0]))
+                self.screen.blit(self.lifebox, (x, 40))
+
     def collision(self):
         if self.enemy:
             for enemy in self.enemy:
                 if pygame.sprite.spritecollide(enemy, self.player, False):
                     enemy.kill()
                     self.life -= 1
+                    if self.life <= 0 :
+                        self.game_status = 0
+
+
         if self.player.sprite.bullet:
             for bullet in self.player.sprite.bullet:
                 if pygame.sprite.spritecollide(bullet, self.enemy, True):
                     pygame.mixer.Sound.play(self.hit)
                     bullet.kill()
-                    self.money += random.randrange(5,100) +self.enemy_kill
-                    self.score += 1000
+                    self.money += random.randrange(5, 70) + self.enemy_kill
+                    self.score += random.randrange(500, 1000,100)
                     self.enemy_kill += 1
 
     # button
     def display_button(self):
         pygame.draw.rect(self.screen, ('black'), self.button_money)
-        display_text(f'{self.money_upgrade_bullet[self.money_upgrade_bullet_i]}$', 18, 'white', (self.screen_width - 110, 100), self.screen)
+        display_text(f'{self.money_upgrade_bullet[self.money_upgrade_bullet_i]}$', 18, 'white', (
+            self.screen_width - 110, 100), self.screen)
 
     def get_mouse_input(self):
         for event in pygame.event.get():
@@ -157,7 +204,7 @@ class Gamerun():
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mx, my = pygame.mouse.get_pos()
                 if self.button_money.collidepoint((mx, my)):
@@ -166,11 +213,12 @@ class Gamerun():
                             self.money -= self.money_upgrade_bullet[self.money_upgrade_bullet_i]
 
                             self.player.sprite.bullet_type += 1
-                            if self.player.sprite.bullet_type  > 5:
-                                self.player.sprite.bullet_type  = 5
+                            if self.player.sprite.bullet_type > 5:
+                                self.player.sprite.bullet_type = 5
 
                             self.money_upgrade_bullet_i += 1
-#20 100 500 1000 5000
+# 20 100 500 1000 5000
+
     def run(self):
         self.screen.fill("white")
 
@@ -190,4 +238,4 @@ class Gamerun():
         self.display_button()
         self.get_mouse_input()
 
-        self.collision()        
+        self.collision()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pygame, sys
+import pygame
+import sys
 from gamerun import Gamerun
 
 pygame.init()
@@ -8,31 +9,82 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 
 gamerun = Gamerun(screen, screen_width, screen_height)
+font = pygame.font.Font('graphics/font/PressStart2P-vaV7.ttf', 20)
 music = pygame.mixer.music.load("graphics/music/bg.mp3")
 pygame.mixer.music.play(-1)
+
+game_status = 1
+prev_player_score = 0
+new_player_score = 0
+
+scores = []
+rankscores = []
+show = 0
 
 def display_text(text, size, color, pos, screen):
     font = pygame.font.Font('graphics/font/PressStart2P-vaV7.ttf', size)
     text_surf = font.render(f'{text}', False, color)
-    text_rect = text_surf.get_rect(center = pos)
+    text_rect = text_surf.get_rect(center=pos)
     screen.blit(text_surf, text_rect)
+
+def draw_text_rank(text, color, size, screen, pos):
+    font = pygame.font.Font('graphics/font/PressStart2P-vaV7.ttf', size)
+    textobj = font.render(text, False, color)
+    textrect = textobj.get_rect(midleft = pos)
+    screen.blit(textobj, textrect)
+
+def ranking():
+    global scores, rankscores, show
+    if show != 1:
+        scores = []
+        rankscores = []
+        with open('score.txt') as file:
+            for line in file:
+                name, score = line.split(',')
+                score = int(score)
+                scores.append((name, score))
+            scores.sort(key=lambda s: s[1])
+            scores.reverse()
+            for num in range(0, 5):
+                rankscores.insert(num,scores[num])
+            file.flush()
+            show = 1
+
+def display_rank():
+    ranking()
+    space = 0
+    for i in range(0, 5):
+        draw_text_rank(f'{rankscores[i][0]}', ('black'), 20, screen, (screen_width / 2 - 250, 230 + space))
+        space += 50
+
+    space = 0
+    for i in range(0, 5):
+        draw_text_rank(f'{rankscores[i][1]}', ('black'), 20, screen, (screen_width / 2 + 200, 230 + space))
+        space += 50
 
 def menu():
     while True:
+        global game_status
+
         screen.fill("white")
 
-        display_text('Cha Lam Chop Ngap Kun', 50, 'black', (screen_width/2, screen_height/2 - 130), screen)
+        display_text('Cha Lam Chop Ngap Kun', 50, 'black',
+                     (screen_width/2, screen_height/2 - 130), screen)
 
-        display_text('65010731 Patsakorn Thong-un', 25, 'black', (screen_width/2, screen_height/2 - 20), screen)
+        display_text('65010731 Patsakorn Thong-un', 25, 'black',
+                     (screen_width/2, screen_height/2 - 20), screen)
 
-        game_button = pygame.Rect((screen_width/2 - 75, screen_height/2 + 70), (150, 60))
+        game_button = pygame.Rect(
+            (screen_width/2 - 75, screen_height/2 + 70), (150, 60))
         pygame.draw.rect(screen, ('black'), game_button)
-        display_text('play', 30, 'white', (screen_width/2, screen_height/2 + 100), screen)
+        display_text('play', 30, 'white', (screen_width /
+                     2, screen_height/2 + 100), screen)
 
-        rank_button = pygame.Rect((screen_width/2 - 75, screen_height/2 + 170), (150, 60))
+        rank_button = pygame.Rect(
+            (screen_width/2 - 75, screen_height/2 + 170), (150, 60))
         pygame.draw.rect(screen, ('black'), rank_button)
-        display_text('rank', 30, 'white', (screen_width/2, screen_height/2 + 200), screen)
-
+        display_text('rank', 30, 'white', (screen_width /
+                     2, screen_height/2 + 200), screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -41,21 +93,54 @@ def menu():
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mx, my = pygame.mouse.get_pos()
                 if game_button.collidepoint((mx, my)):
                     game()
                 if rank_button.collidepoint((mx, my)):
                     rank()
-                
+        
         pygame.display.update()
         clock.tick(60)
 
+
 def game():
     while True:
-        
-        gamerun.run()
+        global game_status, new_player_score, prev_player_score
+
+        new_player_score = gamerun.score
+        if new_player_score >= prev_player_score:
+            prev_player_score = new_player_score
+
+        if game_status == 1:
+            gamerun.run()
+            game_status = gamerun.game_status
+        elif game_status == 0:
+            gameover()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+                   
+        pygame.display.update()
+        clock.tick(60)
+
+
+def rank():
+
+    while True:
+        screen.fill("white")
+        display_text('Rank', 40, 'black', (screen_width/2, 70), screen)
+        menu_button = pygame.Rect(
+            (screen_width - 250, screen_height/2 + 200), (150, 60))
+        pygame.draw.rect(screen, ('black'), menu_button)
+        display_text('back', 30, 'white', (screen_width -
+                     175, screen_height/2 + 200 + 30), screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -65,14 +150,30 @@ def game():
                 pygame.quit()
                 sys.exit()
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mx, my = pygame.mouse.get_pos()
+                if menu_button.collidepoint((mx, my)):
+                    menu()
+
+        display_rank()
+
         pygame.display.update()
         clock.tick(60)
 
 
-def rank():
+def gameover():
+    user_ip = ''
+    text_box = pygame.Rect((screen_width/2 - 350/2, screen_height/2 - 20), (350, 50))
+    active = False
     while True:
+        global game_status, gamerun
+
+        gamerun = Gamerun(screen, screen_width, screen_height)
+
+        game_status = 1
+
         screen.fill("white")
-        display_text('Rank', 40, 'black', (screen_width/2, 70), screen)
+
         menu_button = pygame.Rect((screen_width - 250, screen_height/2 + 200), (150, 60))
         pygame.draw.rect(screen, ('black'), menu_button)
         display_text('back', 30, 'white', (screen_width - 175, screen_height/2 + 200 + 30), screen)
@@ -84,16 +185,46 @@ def rank():
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mx, my = pygame.mouse.get_pos()
+                if text_box.collidepoint(event.pos):
+                    active = True
+                else:
+                    active = False
                 if menu_button.collidepoint((mx, my)):
+                    file = open('score.txt', 'a')
+                    file.write(f'{user_ip}, {prev_player_score}\n')
+                    file.flush()
+                    file.close()
                     menu()
+            
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_BACKSPACE:
+                        user_ip = user_ip[:-1]
+                    else:
+                        user_ip += event.unicode
+                        if surf.get_width() > text_box.w - 20:
+                            user_ip = user_ip[:-1]
+        
+        display_text('GAMEOVER', 40, ('red'), (screen_width / 2, 70), screen)
 
+        display_text(f'SCORE : {prev_player_score}', 20,('black'), (screen_width/2, 180), screen)
+
+        display_text('TYPE YOUR NAME', 20,('black'), (screen_width/2, 300), screen)
+
+        if active:
+            color = pygame.Color('black')
+        else:
+            color = pygame.Color('grey')
+
+        pygame.draw.rect(screen, color, text_box)
+        surf = font.render(user_ip, True, 'white')
+        screen.blit(surf, (text_box.x + 5, text_box.y + 20))
+            
         pygame.display.update()
         clock.tick(60)
 
-def gameover():
-    pass
 
 menu()
